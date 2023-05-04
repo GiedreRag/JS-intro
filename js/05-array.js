d8736caecbd007cef90f9dbac1d780adc33b3a34
Giedre <giedre.rageliene@gmail.com>
2023-05-04 16:39:26 +0300

    Array index, length and strings alike

--- js/05-array.js
+++ js/05-array.js
@@ -1,6 +1,10 @@
 console.clear();
 /*
 ARRAY - arejus (slang), sarasas, listas (slang), matrica, masyvas.
+
+Masyvas prasideda index = 0;
+array.length - masyve esanciu reiksmiu kiekis;
+
 */
 
 const empty = [];
@@ -38,3 +42,65 @@ console.log(marks[1]);
 console.log(marks[2]);
 console.log(marks[3]);
 console.log(marks[4]);
+
+console.clear();
+
+const marks1 = [10, 9, 8];
+const marks2 = [6, 7, 8];
+const marks3 = [5, 5, 5];
+
+function average(marks) {
+    let sum = 0;
+    sum += marks[0];
+    sum += marks[1];
+    sum += marks[2];
+
+    return sum / marks.length;
+}
+
+console.log('Vidurkis 1:', marks1, '=', average(marks1));
+console.log('Vidurkis 2:', marks2, '=', average(marks2));
+console.log('Vidurkis 3:', marks3, '=', average(marks3));
+
+const dict1 = ['pirmas' , 'zodis' , 'labas'];
+const dict2 = ['Labas', 'rytas' , 'Lietuva'];
+const dict3 = ['Labas' , 'rytas' , 'suraitytas'];
+
+// Zodynas: a, b, c.
+
+function dictionary (words) {
+    // console.log(words);
+    let result = 'Zodynas: ';
+
+    result += words[0] + ', ';
+    result += words[1] + ', ';
+    result += words[2] + '. ';
+
+    // arba kitas variantas:
+
+    // result += words[0];
+    // result += ', ';
+    // result += words[1];
+    // result += ', ';
+    // result += words[2];
+    // result += '.';
+
+    // 3cias variants:
+
+
+    return result;
+    
+    // return `Zodynas: ${words[0]}, ${words[1]}, ${words[2]}.`
+}
+
+console.log(dictionary(dict1));
+console.log(dictionary(dict2));
+console.log(dictionary(dict3));
+
+console.clear();
+
+const text = 'Labas rytas';
+console.log(text);
+console.log(text[0]);
+
+console.log(text.length);
