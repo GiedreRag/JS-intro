8db3414f16e6e92818a232974d128892064ab9f1
Giedre <giedre.rageliene@gmail.com>
2023-05-06 18:40:27 +0300

    Ternary

---
+++ js/11-ternary.js
@@ -0,0 +1,37 @@
+console.clear();
+/* 
+TERNARY
+
+klausimas ? teigiama reiksme : neigiama reiksme;
+*/
+
+const leidimas = 10 >= 18 ? 'pilnametis' : 'nepilnametis';
+console.log(leidimas);
+
+// jei ispletus naudojant if:
+let approve = '';
+
+if (10 >= 18) {
+    approve = 'pilnametis';
+} else {
+    approve = 'nepilnametis';
+}
+console.log(approve);
+
+console.log('--------------');
+
+const a = 1 ? 2 : 3;
+console.log(a);
+
+// pavyzdziai, bet tokiu nenaudot:
+const b = 1 ? 2 : 3 ? 4 : 5; 
+console.log(b);
+
+const c = 1 < 2 ? 3 ? 4 : 5 : 6; 
+console.log(c);
+
+const d = 1 > 2 ? 3 : 4 ? 5 : 6; 
+console.log(d);
+
+const e = 1 < 2 ? 3 ? 4 : 5 : 6 ? 7 :8; 
+console.log(e);
