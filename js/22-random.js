62ea96659c50d984b1d9eb9c68c7df389e0a6c8e
Giedre <giedre.rageliene@gmail.com>
2023-05-19 15:23:01 +0300

    commit

---
+++ js/22-random.js
@@ -0,0 +1,19 @@
+/* isskaiciuot random is dvi grupes */
+
+console.clear();
+
+const list = ['a', 'b', 'c', 'd', 'e'];
+
+const final1 = [];
+const final2 = [];
+
+for (const user of list) {
+    if (Math.floor(Math.random() * 2)) {
+        final1.push(user);
+    } else {
+        final2.push(user);
+    }
+}
+
+console.log(final1);
+console.log(final2);
